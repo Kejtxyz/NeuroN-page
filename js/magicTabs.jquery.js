4190949e9212589b458b16b48c3b3fb8133a7774
Bartek Sobieszek <bsobieszek@asis.pl>
2019-02-11 00:02:50 +0100

    Fix removing class

--- js/magicTabs.jquery.js
+++ js/magicTabs.jquery.js
@@ -1,3 +1,14 @@
+var availableGradientClasses = [
+    'gradient-1',
+    'gradient-2',
+    'gradient-3',
+    'gradient-4',
+    'gradient-5',
+    'gradient-6',
+    'gradient-7',
+    'gradient-8'
+];
+
 function estate($element) {
     $('.stateable').removeClass('link-active');
 
@@ -5,6 +16,7 @@ function estate($element) {
 
     var cssClass = $element.data('gradient_class');
     if(typeof cssClass !== "undefined") {
+        $('#main-header').removeClass(availableGradientClasses);
         $('#main-header').addClass(cssClass);
     }
 }
