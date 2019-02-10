9208062415cdc948a2ab15a2a919fa2fdc26e20f
Bartek Sobieszek <bsobieszek@asis.pl>
2019-02-10 22:57:01 +0100

    No ten gradient na zakładkach

--- js/magicTabs.jquery.js
+++ js/magicTabs.jquery.js
@@ -1,4 +1,16 @@
-$(document).on('click', '.stateable', function() {
+function estate($element) {
     $('.stateable').removeClass('link-active');
-    $(this).addClass('link-active');
+
+    $element.addClass('link-active');
+
+    var cssClass = $element.data('gradient_class');
+    if(typeof cssClass !== "undefined") {
+        $('#main-header').addClass(cssClass);
+    }
+}
+
+$(document).on('click', '.stateable', function() {
+    estate($(this));
 });
+
+estate($('#neuron-foundation-href'));
