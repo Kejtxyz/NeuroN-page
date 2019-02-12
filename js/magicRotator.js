c38f920053dfebc17ce364be094170c74406e8d4
Protean <43477801+ProteanCode@users.noreply.github.com>
2019-02-12 23:37:25 +0100

    Backup

--- js/magicRotator.js
+++ js/magicRotator.js
@@ -1,5 +1,3 @@
-var documentComputedStyle   = window.getComputedStyle(window.document.documentElement);
-var documentWidth           = documentComputedStyle.width.replace('px', '');
 var viewport = {
     height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 };
@@ -13,11 +11,12 @@ function getElementData(selector) {
     var bodyBoundingClientRect      = document.body.getBoundingClientRect();
     var elementBoundingClientRect   = element.getBoundingClientRect();
     var top                         = parseInt(elementBoundingClientRect.top - bodyBoundingClientRect.top);
-
+    var animateLeft                 = element.classList.contains('animate-left');
     return {
-        element:    element,
-        height:     height,
-        top:        top,
+        element:            element,
+        height:             height,
+        direction:          animateLeft ? 'left' : 'right',
+        top:                top,
         rotation: {
             max: 15,
             maxAttitude: viewport.height
@@ -31,35 +30,25 @@ var elements = [
     getElementData('#section-container-img-3 > img')
 ];
 
-if(documentWidth >= 1200) {
-    var rotate = function(element) {
-        var currentScrollTop    = document.documentElement.scrollTop;
+var rotate = function(element) {
+    var currentScrollTop    = document.documentElement.scrollTop;
+    var rotationFix         = 9;
+    var currentAttitude     = -1 * (currentScrollTop - (element.top + (element.height/2)));
+    var landingAttitude     = element.rotation.maxAttitude;
+    var maximumLandingAngle = element.rotation.max;
+    var currentLandingAngle = (-1 * ((currentAttitude * maximumLandingAngle) / landingAttitude)) + rotationFix;
 
-        var currentAttitude     = -1 * (currentScrollTop - (element.top + (element.height/2)));
-        var landingAttitude     = element.rotation.maxAttitude;
-        var maximumLandingAngle = element.rotation.max;
-        var currentLandingAngle = -1 * ((currentAttitude * maximumLandingAngle) / landingAttitude);
-        var rotationFix         = 9;
+    if(element.direction === 'left') {
+        currentLandingAngle = -currentLandingAngle;
+    }
 
-        currentLandingAngle += rotationFix;
+    element.element.style.transform = 'rotate(' + currentLandingAngle + 'deg)';
+};
 
-        if(currentLandingAngle < -1 * maximumLandingAngle) {
-            currentLandingAngle = -1 * maximumLandingAngle;
-        }
-        if(currentLandingAngle > maximumLandingAngle) {
-            currentLandingAngle = maximumLandingAngle;
-        }
+var onScroll = function(e) {
+   for(var i = 0; i < elements.length; i++) {
+       rotate(elements[i]);
+   }
+};
 
-        element.element.style.transform = 'rotate(' + currentLandingAngle + 'deg)';
-    };
-
-    var onScroll = function(e) {
-       for(var i = 0; i < elements.length; i++) {
-           rotate(elements[i]);
-       }
-    };
-
-    window.addEventListener('scroll', onScroll);
-}
-
-
+window.addEventListener('scroll', onScroll);
