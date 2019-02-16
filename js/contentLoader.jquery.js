c7e7de1444c4856b209c98e3b26676d6a6b362cc
Bartłomiej Sobieszek <bsobieszek@protean.pl>
2019-02-16 12:21:55 +0100

    fake api

---
+++ js/contentLoader.jquery.js
@@ -0,0 +1,60 @@
+(function($) {
+    var defaults = {
+        rootUrl:        null,
+        primaryUrl:     null,
+        fallbackUrl:    null
+    };
+
+    var settings = defaults;
+
+    $.fn.contentLoader = function(options, url, template, selector, parser, after) {
+        settings = $.extend(defaults, options);
+
+        if(settings.rootUrl === null) {
+            $.ajax(settings.primaryUrl, {
+                success: function() {
+                    settings.rootUrl = settings.primaryUrl;
+                },
+                error: function() {
+                    settings.rootUrl = settings.fallbackUrl;
+                }
+            }).done(function(server) {
+                $.ajax(settings.rootUrl + url, {
+                    success: function(response) {
+
+                        response = parser(response);
+
+                        $.ajax('/templates/' + template + '.html', {
+                            success: function(template) {
+                                var html = Mustache.render(template, response);
+                                $(selector).html(html);
+                                after();
+                            }
+                        });
+                    }
+                });
+            });
+        } else {
+            $.ajax(settings.rootUrl + url, {
+                success: function(response) {
+                    $(selector).addClass("animated fadeOut");
+                    response = parser(response);
+                    $.ajax('/templates/' + template + '.html', {
+                        success: function(template) {
+                            var html = Mustache.render(template, response);
+                            $('html, body').animate({
+                                scrollTop: $(selector).offset().top
+                            }, 1000);
+                            setTimeout(function() {
+                                $(selector).addClass("fadeIn");
+                                $(selector).removeClass("fadeOut");
+                                $(selector).html(html);
+                                after();
+                            }, 500);
+                        }
+                    });
+                }
+            });
+        }
+    };
+})(jQuery);
---
+++ js/contentLoader.js
@@ -0,0 +1,52 @@
+var config = {
+    primaryUrl:     'http://localhost:8095',
+    fallbackUrl:    'https://protean.pl:8088'
+};
+
+function loadNeuronFoundationContent() {
+    $(document).contentLoader(
+        config,
+        '/api/tab/neuron_foundation',
+        'neuron_foundation',
+        '#main',
+        function(response) {
+            $.each(response.articles, function(index) {
+                this.isEven = (index % 2 === 0);
+                this.index  = (index+1);
+            });
+            $.each(response.news, function(index) {
+                this.isEven         = (index % 2 === 0);
+            });
+
+            return {
+                articles:   response.articles,
+                news:       response.news
+            };
+        },
+        function() {
+            $('<script src="js/magicRotator.js"><\/script>').appendTo(document.body);
+        }
+    );
+    return false;
+}
+
+function loadAboutUsContent() {
+    $(document).contentLoader(
+        config,
+        '/api/tab/about_us',
+        'about_us',
+        '#main',
+        function(response) {
+            return response;
+        },
+        function() {
+
+        }
+    );
+    return false;
+}
+
+$(document).ready(loadNeuronFoundationContent);
+
+$(document).on('click', '[data-content_loader="neuron_foundation"]', loadNeuronFoundationContent);
+$(document).on('click', '[data-content_loader="about_us"]', loadAboutUsContent);
