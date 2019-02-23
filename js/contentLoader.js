988bbe7e76d4ede6a9a983cc1994105a409a93de
Bartłomiej Sobieszek <bsobieszek@protean.pl>
2019-02-23 22:57:12 +0100

    Modyfikacja zasięgu szablonów

--- js/contentLoader.js
+++ js/contentLoader.js
@@ -3,11 +3,11 @@ var config = {
     fallbackUrl:    'http://localhost:8095'
 };
 
-function loadNeuronFoundationContent() {
+function loadDefault() {
     $(document).contentLoader(
         config,
         '/api/tab/neuron_foundation',
-        'neuron_foundation',
+        'default_template',
         '#main',
         function(response) {
             $.each(response.articles, function(index) {
@@ -30,6 +30,41 @@ function loadNeuronFoundationContent() {
     return false;
 }
 
+function loadNeuronFoundationContent() {
+    loadDefault();
+    return false;
+}
+
+function loadNewNeuropsychiatryContent() {
+    loadDefault();
+    return false;
+}
+
+function loadNewNeuronDirectionContent() {
+    loadDefault();
+    return false;
+}
+
+function loadOakesInnovateContent() {
+    loadDefault();
+    return false;
+}
+
+function loadNTVChannelContent() {
+    loadDefault();
+    return false;
+}
+
+function loadOakesClinicContent() {
+    loadDefault();
+    return false;
+}
+
+function loadOakAtlantisContent() {
+    loadDefault();
+    return false;
+}
+
 function loadAboutUsContent() {
     $(document).contentLoader(
         config,
@@ -49,4 +84,10 @@ function loadAboutUsContent() {
 $(document).ready(loadNeuronFoundationContent);
 
 $(document).on('click', '[data-content_loader="neuron_foundation"]', loadNeuronFoundationContent);
+$(document).on('click', '[data-content_loader="new_neuropsychiatry"]', loadNewNeuropsychiatryContent);
+$(document).on('click', '[data-content_loader="neuron_direction"]', loadNewNeuronDirectionContent);
+$(document).on('click', '[data-content_loader="oakes_innovate"]', loadOakesInnovateContent);
+$(document).on('click', '[data-content_loader="ntv_channel"]', loadNTVChannelContent);
+$(document).on('click', '[data-content_loader="oakes_clinic"]', loadOakesClinicContent);
+$(document).on('click', '[data-content_loader="oak_atlantis"]', loadOakAtlantisContent);
 $(document).on('click', '[data-content_loader="about_us"]', loadAboutUsContent);
